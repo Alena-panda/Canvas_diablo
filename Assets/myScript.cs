991fd1009a89153a01e4743d19b282dda3c3f652
TEACHER <TEACHER@K-406-3>
2024-02-21 18:12:43 +0700

    написала метод

--- Assets/myScript.cs
+++ Assets/myScript.cs
@@ -7,8 +7,12 @@ public class myScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+
+    //gldkglds,
+
+    void method(){}
 
     // Update is called once per frame
     void Update()
